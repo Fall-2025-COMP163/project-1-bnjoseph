bc7084070cb0ea7ae8a2e994c98293172e721896
bnjoseph <bnjoseph@aggies.ncat.edu>
2025-10-29 23:48:00 -0400

    project code update

--- project1_starter.py
+++ project1_starter.py
@@ -1,11 +1,13 @@
 """
 COMP 163 - Project 1: Character Creator & Saving/Loading
-Name: [Your Name Here]
-Date: [Date]
+Name: [Bryce Joseph]
+Date: [Oct 24, 2025]
 
-AI Usage: [Document any AI assistance used]
+AI Usage: There was no AI used for this project
 Example: AI helped with file I/O error handling logic in save_character function
 """
+
+import os
 
 def create_character(name, character_class):
     """
@@ -18,7 +20,9 @@ def create_character(name, character_class):
     """
     # TODO: Implement this function
     # Remember to use calculate_stats() function for stat calculation
-    pass
+    stats = calculate_stats(character_class, 1)
+    #returns dictionary of character values
+    return {"name" : name, "class" : character_class, "level" : 1, "strength" : stats[0], "magic" : stats[1], "health" : stats[2], "gold" : 100}
 
 def calculate_stats(character_class, level):
     """
@@ -33,7 +37,22 @@ def calculate_stats(character_class, level):
     """
     # TODO: Implement this function
     # Return a tuple: (strength, magic, health)
-    pass
+
+    #initializes stats list depending on character chosen
+    if character_class == "Warrior":
+        stats = [15, 5, 120]
+    elif character_class == "Mage":
+        stats = [5, 15, 80]
+    elif character_class == "Rogue":
+        stats = [10, 10, 40]
+    else:
+        stats = [10, 15, 120]
+    #multiply base stats by level
+    stats[0] *= level
+    stats[1] *= level
+    stats[2] *= level
+    #return stats as tuple instead of list
+    return tuple(stats)
 
 def save_character(character, filename):
     """
@@ -51,7 +70,25 @@ def save_character(character, filename):
     """
     # TODO: Implement this function
     # Remember to handle file errors gracefully
-    pass
+
+    #titles used when writing to the file
+    titles = [
+        "Character Name",
+        "Class",
+        "Level",
+        "Strength",
+        "Magic",
+        "Health",
+        "Gold"]
+    #mac specifically cannot have "/" in a file name, this returns False if / is in the file name
+    if filename.find("/") != -1:
+        return False
+    #writes characteristics of character to file and returns True
+    with open(filename, "w") as file:
+        for i in range(7):
+            file.write(f"{titles[i]}: {list(character.values())[i]}\n")
+    file.close()
+    return True
 
 def load_character(filename):
     """
@@ -60,7 +97,29 @@ def load_character(filename):
     """
     # TODO: Implement this function
     # Remember to handle file not found errors
-    pass
+
+    #keys used to form character dictionary
+    keys = [
+        "name",
+        "class",
+        "level",
+        "strength",
+        "magic",
+        "health",
+        "gold"]
+    #check if path exists
+    if os.path.exists(filename):
+        character = {}
+        with open(filename, "r") as file:
+            #loop to get values for character dictionary
+            for i, line in enumerate(file):
+                key = keys[i]
+                val = line[line.find(": ")+2:line.find("\n")]
+                if val.isdigit():
+                    val = int(val)
+                character[key] = val
+        file.close()
+        return character
 
 def display_character(character):
     """
@@ -78,7 +137,11 @@ def display_character(character):
     Gold: 100
     """
     # TODO: Implement this function
-    pass
+
+    #print the character sheet
+    print("=== CHARACTER SHEET ===")
+    for key, val in character.items():
+        print(f"{key.capitalize()}: {val}")
 
 def level_up(character):
     """
@@ -88,7 +151,13 @@ def level_up(character):
     """
     # TODO: Implement this function
     # Remember to recalculate stats for the new level
-    pass
+
+    #increase level in character dictionary and update stats with calculate_stats then assign stats to character dictionary accordingly
+    character["level"] += 1
+    stats = calculate_stats(character["class"], character["level"])
+    character["strength"] = stats[0]
+    character["magic"] = stats[1]
+    character["health"] = stats[2]
 
 # Main program area (optional - for testing your functions)
 if __name__ == "__main__":
@@ -100,3 +169,9 @@ if __name__ == "__main__":
     # display_character(char)
     # save_character(char, "my_character.txt")
     # loaded = load_character("my_character.txt")
+
+    #testing example
+    char = create_character("TestHero", "Warrior")
+    display_character(char)
+    save_character(char, "my_character.txt")
+    loaded = load_character("my_character.txt")
